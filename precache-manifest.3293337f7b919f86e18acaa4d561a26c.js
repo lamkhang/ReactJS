43362a541c5958be8ca113a4483fa0d276942246
khang.lam <lbkhang102@gmail.com>
2020-01-05 21:34:53 +0700

    Updates

---
+++ precache-manifest.3293337f7b919f86e18acaa4d561a26c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f414c0438b589844409bb1e1f737011a",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "c6d498f310cc1a556977",
+    "url": "/reactjs/static/css/main.56d722c3.chunk.css"
+  },
+  {
+    "revision": "e52c1a9b199ba44eb9f9",
+    "url": "/reactjs/static/js/2.5a8017d7.chunk.js"
+  },
+  {
+    "revision": "c6d498f310cc1a556977",
+    "url": "/reactjs/static/js/main.6f0a21d3.chunk.js"
+  },
+  {
+    "revision": "1235df3cbfbed0914b4e",
+    "url": "/reactjs/static/js/runtime-main.4711a7b0.js"
+  }
+]);
